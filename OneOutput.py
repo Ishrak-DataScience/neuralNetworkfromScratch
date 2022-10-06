afb90d1a8225b77ee535a2b4ae4394e82c1b478b
hasinishrak-datascience <42507424+Ishrak-DataScience@users.noreply.github.com>
2022-10-06 17:13:41 +0900

    first2files

---
+++ OneOutput.py
@@ -0,0 +1,6 @@
+input=[0, 1, 2, 3]
+Weight= [9, 8, 7, 6]
+bias=10
+
+Output= input[0]*Weight[0]+input[1]*Weight[1]+input[2]*Weight[2]+input[3]*Weight[3]+bias
+print(Output)
---
+++ Threeoutputneuron.py
@@ -0,0 +1,12 @@
+input=[0, 1, 2, 3]
+Weight1= [9, 8, 7, 6]
+Weight2= [9, 8, 7, 6]
+Weight3= [9, 8, 7, 6]
+bias1=10
+bias2=20
+bias3=30
+
+Output= [input[0]*Weight1[0]+input[1]*Weight1[1]+input[2]*Weight1[2]+input[3]*Weight1[3]+bias1,
+         input[0]*Weight2[0]+input[1]*Weight2[1]+input[2]*Weight2[2]+input[3]*Weight2[3]+bias2,
+         input[0]*Weight3[0]+input[1]*Weight3[1]+input[2]*Weight3[2]+input[3]*Weight3[3]+bias3,]
+print(Output)
