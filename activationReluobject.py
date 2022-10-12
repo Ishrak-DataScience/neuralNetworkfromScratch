d6ccad5395b1c013214c438e17293e0e4c8f337e
hasinishrak-datascience <42507424+Ishrak-DataScience@users.noreply.github.com>
2022-10-13 00:06:03 +0900

    activationRelu2

--- activationReluobject.py
+++ activationReluobject.py
@@ -1,5 +1,8 @@
 import numpy as np
 np.random.seed(0)
+X=[[0, 1, 2, 3],
+       [2,5,1,2],
+       [1.5,3.3,-.8,0.7]]
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights= 0.10*np.random.randn(n_inputs,n_neurons)
